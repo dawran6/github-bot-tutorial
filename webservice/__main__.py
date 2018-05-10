dee9338ab0beb5c1f3078311ba2191bf852b7d7b
Daw-Ran Liou <dawran6@gmail.com>
2018-05-10 10:58:28 -0400

    Add issue hook

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -11,7 +11,11 @@ router = routing.Router()
 
 @router.register('issues', action='opened')
 async def issue_opened_event(event, gh, *args, **kwargs):
-    pass
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
+
+    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
+    await gh.post(url, data={"body": message})
 
 
 async def main(request):
